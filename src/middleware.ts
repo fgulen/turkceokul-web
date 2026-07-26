bd741fccb19b6bec461396c8d44f64a3038e46b6
fatih gulen <fatih.gulen@outlook.com>
2026-07-26 13:30:32 +0200

    feat: replace favicon.ico with icon.svg and update middleware matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -45,5 +45,5 @@ export default function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.*\\..*).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|icon|sitemap.xml|robots.txt|.*\\..*).*)'],
 };
